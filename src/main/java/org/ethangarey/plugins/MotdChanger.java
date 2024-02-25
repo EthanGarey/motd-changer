ecfa4e4585c75ef9035c2dc269672bb9e3f8e743
EthanGarey <ethgarey@gmail.com>
2024-02-25 17:52:59 -0500

    Version 0.1 release

--- src/main/java/org/ethangarey/plugins/MotdChanger.java
+++ src/main/java/org/ethangarey/plugins/MotdChanger.java
@@ -5,7 +5,6 @@ import jdk.jfr.internal.LogTag;
 import jdk.jfr.internal.Logger;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,44 +15,38 @@ public final class MotdChanger extends JavaPlugin {
 
     public static MotdChanger motdChanger;
 
+    public static void updateConfig(MotdChanger plugin) {
+        File file = new File(plugin.getDataFolder(), "config.yml");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+
+            } catch (IOException e) {
+                Logger.log(LogTag.JFR, LogLevel.ERROR, "An error occurred, cannot save the configuration file");
+                return;
+            }
+            config = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), "config.yml"));
+            config.set("version", 0.1);
+            config.set("motd", "&5&l&keee&aWelcome to fun network :)&5&l&keee");
+            try {
+                config.save(file);
+            } catch (IOException e) {
+                Logger.log(LogTag.JFR, LogLevel.ERROR, "An error occurred, cannot save the configuration file");
+            }
+
+            return;
+        }
+        config = YamlConfiguration.loadConfiguration(file);
+
+    }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
         motdChanger = this;
-        getCommand("motdmanager").setExecutor(new MotdCommands());
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                File file = new File(getDataFolder(), "config.yml");
-                if (!file.exists()) {
-                    saveResource("config.yml", false);
-                    config = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "config.yml"));
-                    config.set("version", 1);
-                    config.set("motd", "&5&l&keee&aWelcome to fun network :)&5&l&keee");
-                    try {
-                        config.save(file);
-                    } catch (IOException e) {
-                        Logger.log(LogTag.JFR, LogLevel.ERROR, "An error occurred, cannot save the configuration file");
-                    }
-
-                    return;
-                }
-                config = YamlConfiguration.loadConfiguration(file);
-            }
-        }.run();
-
+        updateConfig(this);
+        new MotdCommands(this);
         getServer().getPluginManager().registerEvents(new MotdEvents(), this);
-//                new BukkitRunnable() {
-//                    @Override
-//                    public void run() {
-//                        if (config.getInt("version") == 1) {
-//                            if (config.getString("motd").isEmpty()) {
-//                                config.set("motd", "&5&leee&aWelcome to fun network :)&5&leee");
-//
-//                            }
-//                        }
-//                    }
-//                };
 
 
     }
--- src/main/java/org/ethangarey/plugins/MotdCommands.java
+++ src/main/java/org/ethangarey/plugins/MotdCommands.java
@@ -4,19 +4,25 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
-import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class MotdCommands implements CommandExecutor, TabCompleter {
+
+    MotdChanger plugin;
+
+    public MotdCommands(MotdChanger plugin) {
+        this.plugin = plugin;
+        plugin.getCommand("motdmanager").setExecutor(this);
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length == 1) {
             if (args[0].equals("reload")) {
-                MotdChanger.config = YamlConfiguration.loadConfiguration(new File(MotdChanger.motdChanger.getDataFolder(), "config.yml"));
+                MotdChanger.updateConfig(plugin);
                 sender.sendMessage("reload complete");
             }
         }
